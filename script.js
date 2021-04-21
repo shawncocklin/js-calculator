326cf4258848d503a3eb19831f9a5754649665bd
shawncocklin <shawncocklin@gmail.com>
2021-04-21 19:52:20 -0400

    finished basic JS 4 function calc logic

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const rs = require('readline-sync');
+
+let selectedOperation = rs.question('What math operation would you like to perform? Please choose from /, *, +, or -: ', {limit: ['/', '*', '+', '-'], limitMessage: 'Sorry, please input a valid math operation, like /, *, +, or -: '});
+
+console.log(`${selectedOperation} it is then!`);
+
+let firstNumber = rs.questionInt('Please type in the first number: ');
+
+let secondNumber = rs.questionInt('Please type in the second number: ');
+
+let result;
+if (selectedOperation === '*') {
+  result = firstNumber * secondNumber;
+}
+if (selectedOperation === '/') {
+  result = firstNumber / secondNumber;
+}
+if (selectedOperation === '+') {
+  result = firstNumber + secondNumber;
+}
+if (selectedOperation === '-') {
+  result = firstNumber - secondNumber;
+}
+
+console.log(`The result is ${result}`);
+
